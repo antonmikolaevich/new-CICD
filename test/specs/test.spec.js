9691b787ab5bbfebe2b8903c8c106bb008242c16
Iryna Sikirytskaya <iryna_sikirytskaya@epam.com>
2023-02-09 23:01:47 +0300

    UI_tests_part2

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -10,8 +10,11 @@ describe("Test suite", () => {
     });
 
     it("Check that email is mandatory field", async () =>{
+        browser.waitUntil($(".sidebar-item").$$("doctors").isExisting() === true, {timeout:3000, timeoutMsg: "the element doesn't exist"})
         await $("div.doctors").click();
+        browser.waitUntil($("//div[text()='DOCTORS LIST']").isDisplayed() === true, {timeout:3000})
         await $("//button[text()='Add New Doctor']").click();
+        browser.waitUntil($("div#_title").isExisting() === true, {timeout:3000})
         await $("input[name='Name']").addValue("John Doe");
         await $("//button[text()='Save']").click();
  
@@ -20,8 +23,11 @@ describe("Test suite", () => {
      });
 
      it("Check that name is mandatory field", async () =>{
+        browser.waitUntil($("div.doctors").isExisting() === true, {timeout:3000})
         await $("div.doctors").click();
+        browser.waitUntil($("//button[text()='Add New Doctor']").isClicable() === true, {timeout:5000})
         await $("//button[text()='Add New Doctor']").click();
+        browser.waitUntil($("div#_title").isExisting() === true, {timeout:3000, timeoutMsg: "window is not loaded"})
         await $("input[name='Email']").addValue("johndoe@gmail.com");
         await $("//button[text()='Save']").click();
  
@@ -29,9 +35,27 @@ describe("Test suite", () => {
         expect(await emailError.getText()).toEqual("Enter valid name")
      });
 
-    it("Add new patient", async () =>{
+      it("Check that mobile number is mandatory field", async () =>{
+        browser.waitUntil($(".sidebar-item").$$("patients").isExisting() === true, {timeout:3000})
         await $("div.patients").click();
+        browser.waitUntil($("//div[text()='PATIENT LIST']").isDisplayed() === true, {timeout:3000, timeoutMsg: "element is not displayed"})
         await $("//button[text()='Add New Patient']").click();
+        browser.waitUntil($("//div[text()='New Patient']").isExisting() === true, {timeout:5000})
+        await $("input[name='Name']").addValue("Tom Wegk");
+        await $("//input[@name='Email']").addValue("tomwegk@gmail.com");
+        await $("input[name='Mobile']").clearValue();
+        await $("//button[text()='Save']").click();
+ 
+        const emailError = await $("label#undefined-info");
+        expect(await emailError.getText()).toEqual("Enter valid mobile number")
+     });
+
+    it("Add new patient", async () =>{
+        browser.waitUntil($("div.patients").isExisting() === true, {timeout:3000})
+        await $("div.patients").click();
+        browser.waitUntil($("//div[text()='PATIENT LIST']").isDisplayed() === true, {timeout:3000, timeoutMsg: "element is not displayed"})
+        await $("//button[text()='Add New Patient']").click();
+        browser.waitUntil($("div#_dialog-header").isExisting() === true, {timeout:3000})
         await $("input[name='Name']").addValue("Tim Walker");
         await $("//input[@id='DOB_input']").setValue("01/01/2001");
         await $("//input[@name='Email']").addValue("timwalker@gmail.com");
@@ -43,9 +67,13 @@ describe("Test suite", () => {
      });
 
      it("Edit Doctor Details", async () =>{
+        browser.waitUntil($("div.doctors").isExisting() === true, {timeout:3000})
         await $("div.doctors").click();
+        browser.waitUntil($("//div[text()='DOCTORS LIST']").isDisplayed() === true, {timeout:3000})
         await $("div[id='Specialist_1']").click();
+        browser.waitUntil($("//div[text()='DOCTORS DETAILS']").isDisplayed() === true, {timeout:3000, timeoutMsg: "element is not displayed"})
         await $("//button[text()='Edit']").click();
+        browser.waitUntil($("div#_title").isExisting() === true, {timeout:3000})
         await $("input[name='Designation']").clearValue();
         await $("input[name='Designation']").addValue('Lead Specialist');
         await $("//button[text()='Save']").click();
@@ -53,16 +81,48 @@ describe("Test suite", () => {
         expect(await designation.getText()).toEqual("Lead Specialist")
      });
 
+     it("Delete a patient", async () =>{
+      browser.waitUntil($("div.patients").isExisting() === true, {timeout:3000})
+      await $("div.patients").click();
+      browser.waitUntil($("//div[text()='PATIENT LIST']").isDisplayed() === true, {timeout:5000})
+      await $("//span[text()='Milka']").click();
+      browser.waitUntil($("button[title='Delete']").isClicable() === true, {timeout:5000, timeoutMsg: "element is not clicable"})
+      await $("button[title='Delete']").click();
+      browser.waitUntil($("//div[text()=' Are you sure you want to delete this patient? ']").isClicable() === true, {timeout:3000})
+      await $("//button[text()='Ok']").click();
+      const cancelation = await $("//span[text()='Milka']").waitForDisplayed({reverse: true});
+      expect(cancelation).toEqual(true)
+   });
+
      it("Cancel Appointment", async () =>{
+        browser.waitUntil($("//span[text()='Schedule']").isDisplayed() === true, {timeout:3000})
         await $("//span[text()='Schedule']").click();
-        await browser.pause(3000);
+        browser.waitUntil($(".e-appointment").$$("Appointment_1013").isClicable() === true, {timeout:3000, timeoutMsg: "element is not clicable"})
         await $("//div[@data-id='Appointment_1013']").click();
+        browser.waitUntil($("button[title='Delete']").isClicable() === true, {timeout:3000})
         await $("button[title='Delete']").click();
+        browser.waitUntil($("div#QuickDialog_dialog-content").isClicable() === true, {timeout:3000})
         await $(".e-control button[aria-label='Delete']").click();
-        await browser.pause(3000);
         const cancelation = await $("//div[@data-id='Appointment_1013']").waitForExist({reverse: true});
         expect(cancelation).toEqual(true)
      });
 
+     it("Change calendar view", async () =>{
+      await $("//span[text()='Schedule']").click();
+      browser.waitUntil($("app-calendar").isDisplayed() === true, {timeout: 5000})
+      await $("//span[text()='Month']").click();
+      const changeCalendar = await $("//span[text()='August 2020']").waitForExist();
+      expect(changeCalendar).toEqual(true)
+   });
+
+   it("Search for patient", async () =>{
+      await $("div.patients").click();
+      browser.waitUntil($("input#schedule_searchbar").isClicable() === true, {timeout: 5000});
+      await $("input#schedule_searchbar").setValue('Mercy');
+      browser.waitUntil($("//*[@id='grid_1627125836_2']").isExisting() === true);
+      const foundPatient = await $("//span[@class='patient-name']").getText();
+      expect(foundPatient).toEqual("Mercy")
+   });
 });
+
 
