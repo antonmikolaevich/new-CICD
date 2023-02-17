e6671de1c348a9b8d17aa3d4dee12c9122d4682b
Iryna Sikirytskaya <iryna_sikirytskaya@epam.com>
2023-02-17 15:40:30 +0300

    UI_tests_part4

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+const {existsSync, mkdirSync} = require("fs");
+//const allure = require('allure-commandline');
 exports.config = {
     //
     // ====================
@@ -112,7 +114,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['chromedriver', 'geckodriver'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -134,9 +136,16 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
-
-
+    reporters: ['spec', ['junit', {
+        outputDir: "./report",
+        outputFileFormat: function (options) {
+            return `results-${options.cid}.xml`
+        }
+    }], ['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+    }]],
     
     //
     // Options to be passed to Mocha.
@@ -243,9 +252,18 @@ exports.config = {
      * @param {Boolean} result.passed    true if test has passed, otherwise false
      * @param {Object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
-
+    afterTest: async (test, context, {error, result, duration, passed, retries}) => {
+        if (error) {   
+            const filename = test.title + '.png';     
+            const dirPath = './screenshots/';     
+            if (!existsSync(dirPath)) {
+              mkdirSync(dirPath, {
+                recursive: true,
+              });
+            }    
+            await browser.saveScreenshot(dirPath + filename);
+          }
+        },
 
     /**
      * Hook that gets executed after the suite has ended
@@ -287,8 +305,21 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
      */
-    // onComplete: function(exitCode, config, capabilities, results) {
-    // },
+    onComplete: function() {
+        const reportError = new Error('Could not generate Allure report');
+        const generation = allure(['generate', 'allure-results', '--clean']);   
+        return new Promise((resolve, reject) => {   
+          const generationTimeout = setTimeout(() => reject(reportError), 5000);
+          generation.on('exit', function (exitCode) {
+            clearTimeout(generationTimeout);  
+            if (exitCode !== 0) {    
+              return reject(reportError)    
+            }   
+            console.log('Allure report successfully generated');   
+            resolve();   
+          });   
+        });   
+      },  
     /**
     * Gets executed when a refresh happens.
     * @param {String} oldSessionId session ID of the old session
